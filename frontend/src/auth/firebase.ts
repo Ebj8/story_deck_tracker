9b7f3328b3ef02b5cee785531a3076861e8b02c2
Eli Johnson <ebj800@gmail.com>
2025-01-31 23:44:48 -0700

    reinstalled firebase and moved api key to .env file (the key has been rotated on google cloud)

--- frontend/src/auth/firebase.ts
+++ frontend/src/auth/firebase.ts
@@ -1,16 +1,18 @@
 // Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCSv7hdCe6GT2W_c-82DAEagmM6yKb57mA",
-  authDomain: "sd-tracker-449515.firebaseapp.com",
-  projectId: "sd-tracker-449515",
-  storageBucket: "sd-tracker-449515.firebasestorage.app",
-  messagingSenderId: "691053305129",
-  appId: "1:691053305129:web:e67b924f4d8933a009ab38"
+  apiKey: import.meta.env.FIREBASE_API_KEY,
+  authDomain: import.meta.env.FIREBASE_DOMAIN,
+  projectId: import.meta.env.FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.FIREBASE_APP_ID
 };
 
 // Initialize Firebase
