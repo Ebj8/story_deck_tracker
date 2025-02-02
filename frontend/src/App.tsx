75e975088f649029c9b9e81cd9b32f6e2b4f8a97
Eli Johnson <ebj800@gmail.com>
2025-02-01 20:40:52 -0700

    Now require the user to be authenticated before showing them any content.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,18 +3,38 @@ import AboutPage from "@/pages/about/page";
 import LoginPage from "@/pages/auth/page";
 import { Routes, Route } from "react-router";
 import { Layout } from "@/pages/layout";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useEffect, useState } from "react";
 import "./index.css";
 
 function App() {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const auth = getAuth();
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+
+    // Cleanup the listener on unmount
+    return () => unsubscribe();
+  }, []);
+
   return (
     <>
       <Routes>
         <Route element={<Layout />}>
-          <Route path="/" element={<CollectionPage />} />
-          <Route path="/collection" element={<CollectionPage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/auth" element={<LoginPage />} />
-          <Route path="*" element={<h1>Page Not Found</h1>} />
+          {user ? (
+            <>
+              <Route path="/" element={<CollectionPage />} />
+              <Route path="/collection" element={<CollectionPage />} />
+              <Route path="/about" element={<AboutPage />} />
+              <Route path="/auth" element={<LoginPage />} />
+              <Route path="*" element={<h1>Page Not Found</h1>} />
+            </>
+          ) : (
+            <Route path="*" element={<LoginPage />} />
+          )}
         </Route>
       </Routes>
     </>
