ccbd9ef99eb20691276985fa181176bb32cb8770
Eli Johnson <ebj800@gmail.com>
2025-02-03 21:44:41 -0700

    Now only color the cards that are in the user's collection

--- frontend/src/pages/collection/cardBox.tsx
+++ frontend/src/pages/collection/cardBox.tsx
@@ -1,18 +1,33 @@
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
-import { CatalogCardRead } from "@/requests/gen/react-query/fastAPI.schemas";
+import {
+  CatalogCardRead,
+  CollectionCounts,
+} from "@/requests/gen/react-query/fastAPI.schemas";
 
 interface CardBoxProps {
   card: CatalogCardRead;
+  collection: CollectionCounts[] | undefined;
 }
 
 const CardBox = (props: CardBoxProps) => {
   const [colored, setColored] = useState(false);
+  const filteredCollection = props.collection?.filter(
+    (item) => item.card_id === props.card.card_id
+  );
+
+  useEffect(() => {
+    if (filteredCollection && filteredCollection.length > 0) {
+      setColored(true);
+    }
+  });
 
   const imageUrl =
     props.card.img_front_url ||
     "https://firebasestorage.googleapis.com/v0/b/sd-tracker-449515.firebasestorage.app/o/card_images%2Fplaceholder_image.jpeg?alt=media&token=0de3b6fe-c7eb-4ab4-9a7c-49d1aadb95a5";
+
+  console.log(filteredCollection);
 
   return (
     <Card className="m-4 w-[100%]">
--- frontend/src/pages/collection/page.tsx
+++ frontend/src/pages/collection/page.tsx
@@ -1,8 +1,12 @@
 import CardBox from "./cardBox";
 import { useGetCatalogCards } from "@/requests/gen/react-query/catalog";
+import { useGetCollectionCount } from "@/requests/gen/react-query/collection";
+import { useUser } from "@/auth/userContext";
 
 const CollectionPage = () => {
   const { data } = useGetCatalogCards();
+  const { user } = useUser();
+  const { data: collection } = useGetCollectionCount(user?.uid || "");
 
   const numCards = data?.length || 0;
   const numRows = Math.ceil(numCards / 3);
@@ -11,9 +15,9 @@ const CollectionPage = () => {
     <div>
       {Array.from({ length: numRows }).map((_, rowIndex) => (
         <div key={rowIndex} className="flex flex-row items-center">
-          {data?.slice(rowIndex * 3, rowIndex * 3 + 3).map((card, index) => (
-            <CardBox key={index} card={card} />
-          ))}
+          {data?.slice(rowIndex * 3, rowIndex * 3 + 3).map((card, index) => {
+            return <CardBox key={index} card={card} collection={collection} />;
+          })}
         </div>
       ))}
     </div>
