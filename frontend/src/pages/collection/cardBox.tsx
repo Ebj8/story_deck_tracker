1f1f46a30d88dee586fdc488e43b6e3fc977649c
Eli Johnson <ebj800@gmail.com>
2025-02-06 21:20:46 -0700

    creates first row of collection on setting quantity to 1 from 0

--- frontend/src/pages/collection/cardBox.tsx
+++ frontend/src/pages/collection/cardBox.tsx
@@ -6,7 +6,8 @@ import {
   CatalogCardRead,
   CollectionCounts,
 } from "@/requests/gen/react-query/fastAPI.schemas";
-import { useUser } from "@/auth/userContext";
+import { useUser } from "@/auth/UserContext";
+import { useCreateCollectionRow } from "@/requests/gen/react-query/collection";
 
 interface CardBoxProps {
   card: CatalogCardRead;
@@ -30,6 +31,8 @@ const CardBox = ({ card, collection }: CardBoxProps) => {
   const [regQty, setRegQty] = useState(() => regCollection?.qty ?? 0);
   const [foilQty, setFoilQty] = useState(() => foilCollection?.qty ?? 0);
 
+  const mutation = useCreateCollectionRow();
+
   // Update state only when collection changes
   useEffect(() => {
     setRegQty(regCollection?.qty ?? 0);
@@ -41,6 +44,57 @@ const CardBox = ({ card, collection }: CardBoxProps) => {
     setColored(regQty > 0 || foilQty > 0);
   }, [regQty, foilQty]);
 
+  const handleRegIncrease = () => {
+    if (regQty === 0 && user != undefined) {
+      mutation.mutate(
+        {
+          data: {
+            user_id: user?.uid,
+            card_id: card.card_id,
+            is_foil: false,
+            qty: 1,
+            condition: "NM",
+          },
+        },
+        {
+          onSuccess: (response) => {
+            console.log("Set created successfully!", response);
+          },
+          onError: (error) => {
+            console.error("Error creating set:", error);
+          },
+        }
+      );
+    }
+    setRegQty((prev) => prev + 1);
+  };
+
+  const handleFoilIncrease = () => {
+    if (foilQty === 0 && user != undefined) {
+      mutation.mutate(
+        {
+          data: {
+            user_id: user?.uid,
+            card_id: card.card_id,
+            is_foil: true,
+            qty: 1,
+            condition: "NM",
+          },
+        },
+        {
+          onSuccess: (response) => {
+            console.log("Set created successfully!", response);
+          },
+          onError: (error) => {
+            console.error("Error creating set:", error);
+          },
+        }
+      );
+    }
+    setFoilQty((prev) => prev + 1);
+  };
+
+  // Default image if no image is provided
   const imageUrl =
     card.img_front_url ||
     "https://firebasestorage.googleapis.com/v0/b/sd-tracker-449515.firebasestorage.app/o/card_images%2Fplaceholder_image.jpeg?alt=media&token=0de3b6fe-c7eb-4ab4-9a7c-49d1aadb95a5";
@@ -71,13 +125,13 @@ const CardBox = ({ card, collection }: CardBoxProps) => {
             <Counter
               label="Regular"
               count={regQty}
-              onIncrement={() => setRegQty((prev) => prev + 1)}
+              onIncrement={handleRegIncrease}
               onDecrement={() => setRegQty((prev) => Math.max(0, prev - 1))}
             />
             <Counter
               label="Foil"
               count={foilQty}
-              onIncrement={() => setFoilQty((prev) => prev + 1)}
+              onIncrement={handleFoilIncrease}
               onDecrement={() => setFoilQty((prev) => Math.max(0, prev - 1))}
             />
           </div>
