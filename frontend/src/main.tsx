8264fbe50e384f341a9ad8944c5c9bad7848f946
Eli Johnson <ebj800@gmail.com>
2025-06-29 16:15:36 -0600

    fixed typo

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { UserProvider } from "@/auth/userContext";
+import { UserProvider } from "@/auth/UserContext";
 
 const queryClient = new QueryClient();
 
