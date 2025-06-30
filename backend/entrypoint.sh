7db5b3672f669aa116b2fe688e9212660b0f19e6
Eli Johnson <ebj800@gmail.com>
2025-06-29 22:38:35 -0600

    just pushing the entrypoint.sh to update the repo

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,7 +2,6 @@
 
 # No need to "activate" the environment; just rely on PATH
 echo "Alembic location: $(which alembic)"
-echo "Uvicorn location: $(which uvicorn)"
 
 # Run Alembic migrations
 alembic upgrade head
@@ -10,6 +9,7 @@ if [ $? -ne 0 ]; then
     echo "Migrations failed"
     exit 1
 fi
+
 
 # Start the FastAPI app
 uvicorn app.main:app --host 0.0.0.0 --port ${PORT:-8080}
