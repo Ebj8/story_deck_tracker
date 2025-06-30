cf0ccf3bbe4a8c46bb437a055393db257ee75686
Eli Johnson <ebj800@gmail.com>
2025-06-29 21:47:47 -0600

    another try

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,17 +1,15 @@
 #!/bin/sh
-. /code/.venv/bin/activate
 
+# No need to "activate" the environment; just rely on PATH
 echo "Alembic location: $(which alembic)"
 echo "Uvicorn location: $(which uvicorn)"
 
 # Run Alembic migrations
 alembic upgrade head
-# Exit if migrations fail
 if [ $? -ne 0 ]; then
     echo "Migrations failed"
     exit 1
 fi
 
-# Run the app
-# Note: We don't want extra workers here because we are running multiple containers
+# Start the FastAPI app
 uvicorn app.main:app --host 0.0.0.0 --port ${PORT:-8080}
